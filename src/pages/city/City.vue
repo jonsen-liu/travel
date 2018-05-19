3b280461f80454e820a3ebdeff1de0eaa3758dfb
刘永洁 <1371737867@qq.com>
2018-05-20 00:07:14 +0800

    add city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,12 +1,17 @@
 <template>
-  <city-header></city-header>
+  <div class="city">
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 <script>
 import CityHeader from './components/CityHeader'
+import CitySearch from './components/CitySearch'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="city-search">
+    <input type="text" placeholder="输入城市或拼音搜素">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.city-search {
+  margin-top -.02rem
+  background-color $bgColor
+  padding 0 .1rem .1rem
+  input {
+    width 100%
+    height .6rem
+    font-size .32rem
+    text-align center
+    border-radius .1rem
+    padding 0 .2rem
+    color #666
+    box-sizing border-box
+  }
+}
+</style>
