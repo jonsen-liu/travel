46ed728db1e7a81f8243c98e851d01ba5b9a16fd
刘永洁 <1371737867@qq.com>
2018-05-31 22:37:26 +0800

    添加公共组件画廊

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="gallery" @click="galleryClick">
+    <div class="gallery-center">
+      <swiper :options="swiperOptions">
+        <swiper-slide v-for="(item,index) of imgs" :key="index">
+          <img class="gallery-img" :src="item">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'commonGallery',
+  props: {
+    imgs: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    galleryClick () {
+      this.$emit('galleryclick')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .gallery-center >>> .swiper-container
+    overflow visible
+  .swiper-pagination
+    color #fff
+    bottom -1rem
+  .gallery
+    display flex
+    flex-flow column
+    justify-content center
+    position absolute
+    z-index 99
+    top 0
+    right 0
+    left 0
+    bottom 0
+    background-color #000
+    .gallery-center
+      width 100%
+      .gallery-img
+        width 100%
+</style>
--- src/pages/details/components/banner.vue
+++ src/pages/details/components/banner.vue
@@ -1,13 +1,45 @@
 <template>
-  <div class="banner">
-    <img src="https://img1.qunarzz.com/sight/p0/1707/ab/abb70c851caaa6d8a3.img.jpg_600x330_af719163.jpg">
+  <div>
+    <div class="banner" @click="bannerClick">
+      <img src="https://img1.qunarzz.com/sight/p0/1707/ab/abb70c851caaa6d8a3.img.jpg_600x330_af719163.jpg">
+      <div class="banner-title">
+        <div class="banner-icone">
+          <span class="iconfont">&#xe605;</span>
+          47
+        </div>
+        长隆水上乐园(AAAAA景区)
+      </div>
+    </div>
+    <common-gallery v-show="galleryShow" @galleryclick="galleryClick" :imgs="imgs"></common-gallery>
   </div>
 </template>
 
 <script>
 // import { mapState } from 'vuex'
+import commonGallery from 'common/gallery/Gallery'
 export default {
-  name: 'detailBanner'
+  name: 'detailBanner',
+  components: {
+    commonGallery
+  },
+  data () {
+    return {
+      galleryShow: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1707/ab/abb70c851caaa6d8a3.img.jpg_r_800x800_f1e628fe.jpg',
+        'http://img1.qunarzz.com/sight/p0/1707/ee/ee7ddb170be69716a3.img.jpg_r_800x800_f0ad7d83.jpg',
+        'http://img1.qunarzz.com/sight/p0/1707/33/33d47d55bbab5c84a3.img.jpg_r_800x800_a9489697.jpg'
+      ]
+    }
+  },
+  methods: {
+    bannerClick () {
+      this.galleryShow = true
+    },
+    galleryClick () {
+      this.galleryShow = false
+    }
+  }
 }
 </script>
 
@@ -18,6 +50,28 @@ export default {
     overflow hidden
     height 0
     padding-bottom 55%
+    position relative
     img
       width 100%
+    .banner-title
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      color #fff
+      height 1.2rem
+      font-size .36rem
+      padding-left .2rem
+      background-image linear-gradient(rgba(0,0,0,0),rgba(0,0,0,.12),rgba(0,0,0,.3),rgba(0,0,0,.8))
+      .banner-icone
+        width 1.2rem
+        height 0.4rem
+        line-height .4rem
+        font-size .2rem
+        text-align center
+        margin 0 0 .1rem
+        background-color rgba(0, 0, 0, .6)
+        border-radius .18rem
+        .iconfont
+          font-size .16rem
 </style>
